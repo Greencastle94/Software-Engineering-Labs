312d4e6b71306f7b65340b4ab0b8c4bb19f71f52
Lucas Grönborg <lucas94gronborg@gmail.com>
2017-05-11 10:55:14 +0200

    Added constructor, setters and getters for MyNode

--- lab5/src/MyNode.java
+++ lab5/src/MyNode.java
@@ -1,5 +1,28 @@
 import javax.swing.tree.DefaultMutableTreeNode;
 
 public class MyNode extends DefaultMutableTreeNode {
+    private String level;
+    private String text;
 
+    MyNode(String lvl, String txt) {
+        super();
+        level = lvl;
+        text = txt;
+    }
+
+    public void setText(String txt) {
+        text = txt;
+    }
+
+    public void setLevel(String lvl) {
+        level = lvl;
+    }
+
+    public String getLvl() {
+        return level;
+    }
+
+    public String getTxt() {
+        return text;
+    }
 }
