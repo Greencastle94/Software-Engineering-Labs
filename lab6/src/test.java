aafe41288a23591783c09d0c70f3b511229062d1
RickardBjorklund <draqir@gmail.com>
2017-05-15 10:37:00 +0200

    made som changes to test

--- lab6/src/test.java
+++ lab6/src/test.java
@@ -2,6 +2,8 @@ public class test {
 
     public static void main(String[] args){
         System.out.println("Hej");
+        System.out.println("Hej igen");
+        System.out.println("Hej ännu en gång");
     }
 
 }
