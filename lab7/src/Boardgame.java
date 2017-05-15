adf42b23bd43e6772ce8b0c940425c7cda50826b
Lucas Grönborg <lucas94gronborg@gmail.com>
2017-05-15 16:26:58 +0200

    Added Boardgame interface

---
+++ lab7/src/Boardgame.java
@@ -0,0 +1,7 @@
+// Rickard Björklund & Lucas Grönborg
+
+public interface Boardgame {
+    public boolean move(int i, int j); //ger true om draget gick bra, annars false
+    public String getStatus(int i, int j);
+    public String getMessage();
+}
